65f73c82c00de50ad458760fbccf954a59ddb816
Emil <emil.kais94@gmail.com>
2019-05-30 10:35:47 -0400

    Removed keyInfo.

--- app/config/index.js
+++ app/config/index.js
@@ -35,12 +35,4 @@ winston.stream({ start: -1 }).on('error', function(log) {
 module.exports = {
 	secret : 'e72257a03bb6fd583bc7e28dacg1c01b211417805126e836ec2631d1a56d3422',
 	logger,
-	cloudant: {
-		account: '5889e324-25b7-4501-bf23-ccbc70434d3f-bluemix',
-		plugins: {
-			iamauth: {
-				iamApiKey: '5N6Xgo3H6Yn5-4bvpU-y3mbLSfVjGyZoAL8sKg1_11KU'
-			}
-		}
-	}
 };
--- app/couchdb/index.js
+++ app/couchdb/index.js
@@ -2,27 +2,27 @@
 
 const logger = require('../config/').logger;
 
-const agentkeepalive = require('agentkeepalive');
-const myagent = new agentkeepalive({
-  maxSockets: 50,
-  maxKeepAliveRequests: 0,
-  maxKeepAliveTime: 30000
-});
+// const agentkeepalive = require('agentkeepalive');
+// const myagent = new agentkeepalive({
+//   maxSockets: 50,
+//   maxKeepAliveRequests: 0,
+//   maxKeepAliveTime: 30000
+// });
 
-const couch = require('nano')({
-    url: 'http://localhost:5984/',
-    requestDefaults : { 'agent' : myagent },
-    db: 'testa'
-  });
+// const couch = require('nano')({
+//     url: 'http://localhost:5984/',
+//     requestDefaults : { 'agent' : myagent },
+//     db: 'testa'
+//   });
 
-exports.createDatabase = (name) => {
-    couch.createDatabase(name)
-        .then((done) => {
-            logger.info(done);
-        })
-        .catch((err) => {
-            logger.error(err);
-        });    
-};
+// exports.createDatabase = (name) => {
+//     couch.createDatabase(name)
+//         .then((done) => {
+//             logger.info(done);
+//         })
+//         .catch((err) => {
+//             logger.error(err);
+//         });    
+// };
 
 module.exports = couch;
